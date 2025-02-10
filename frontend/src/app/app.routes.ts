db76e89f21839e21570df1329ddd985f1ecc2787
Andrii Seleznov <andriiseleznovukr@gmail.com>
2025-02-10 20:51:45 +0100

    Added routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', pathMatch: 'full', component: DashboardComponent },
+	{ path: '**', component: PageNotFoundComponent }
+];
