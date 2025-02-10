514f184402e4914150510257368c4ea1cdffd03b
Andrii Seleznov <andriiseleznovukr@gmail.com>
2025-02-10 20:36:28 +0100

    Add daisyui

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+const { default: daisyui } = require('daisyui');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html,ts}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("daisyui")],
+	daisyui:{
+		themes:["light", "dark"]
+	}
+};
+
