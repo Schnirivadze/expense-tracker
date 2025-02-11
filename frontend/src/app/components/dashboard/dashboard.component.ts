62b948b3061ce371a857af6700adcebf25bc529d
Andrii Seleznov <andriiseleznovukr@gmail.com>
2025-02-11 16:11:04 +0100

    Created dashboard layout

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
+import { CurrencyPipe, DatePipe, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard',
-  imports: [],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+	selector: 'app-dashboard',
+	imports: [CurrencyPipe, NgClass, DatePipe],
+	templateUrl: './dashboard.component.html',
+	styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
 
