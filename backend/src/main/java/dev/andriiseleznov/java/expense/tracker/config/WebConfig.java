ea96bd882012c117be835890279def34562447d6
Andrii Seleznov <andriiseleznovukr@gmail.com>
2025-02-11 19:10:52 +0100

    Change port 3000 to 4200

--- backend/src/main/java/dev/andriiseleznov/java/expense/tracker/config/WebConfig.java
+++ backend/src/main/java/dev/andriiseleznov/java/expense/tracker/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Enable CORS for all endpoints
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
                 .allowedHeaders("*")
                 .allowCredentials(true); 
