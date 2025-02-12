816bb8291f84df91bbef230a58e25840cba502a4
Andrii Seleznov <andriiseleznovukr@gmail.com>
2025-02-12 17:24:47 +0100

    Edited dashboard  to show actual data and added a graph

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,20 @@
 import { CurrencyPipe, DatePipe, NgClass } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { TransactionsService } from '../../services/transactions.service';
+import { Transaction } from '../../../model/transaction.type';
+import { Chart, CategoryScale, LinearScale, Title, Tooltip, Legend, LineElement, PointElement, ArcElement, LineController } from 'chart.js';
+
+Chart.register(
+	CategoryScale,
+	LinearScale,
+	Title,
+	Tooltip,
+	Legend,
+	LineElement,
+	PointElement,
+	ArcElement,
+	LineController
+);
 
 @Component({
 	selector: 'app-dashboard',
@@ -8,5 +23,107 @@ import { Component } from '@angular/core';
 	styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+	transactionsService = inject(TransactionsService)
 
+	transactions = signal<Transaction[]>([]);
+
+	totalBalance = computed(() => this.transactions().reduce((sum, transaction) => sum + transaction.amount, 0))
+
+	ngOnInit() {
+		this.transactionsService.getAllTransactions().subscribe({
+		  next: (data) => {
+			const sortedTransactions = data.sort((a: Transaction, b: Transaction) =>
+			  new Date(b.date).getTime() - new Date(a.date).getTime()
+			);
+			this.transactions.set(sortedTransactions);
+			console.log('Sorted Transactions:', this.transactions());
+			
+			const now = new Date();
+			
+			const dailyBalance: number[] = [];
+			const labels: string[] = [];
+	  
+			let cumulativeBalance = 0;
+			for (let day = 1; day <= now.getDate(); day++) {
+			  const currentDate = new Date(now.getFullYear(), now.getMonth(), day);
+			  
+			  const transactionsForTheDay = sortedTransactions.filter((transaction: Transaction) => {
+				const transactionDate = new Date(transaction.date);
+				return transactionDate.getDate() === day && transactionDate.getMonth() === now.getMonth() && transactionDate.getFullYear() === now.getFullYear();
+			  });
+			  
+			  transactionsForTheDay.forEach((transaction: Transaction) => {
+				cumulativeBalance += transaction.amount;
+			  });
+	  
+			  dailyBalance.push(cumulativeBalance);
+			  labels.push(currentDate.toLocaleDateString('en-GB', { day: 'numeric' }));
+			}
+	  
+			const ctx = (document.getElementById('myChart') as HTMLCanvasElement).getContext('2d');
+			if (ctx) {
+			  const chartData = {
+				labels: labels, 
+				datasets: [{
+				  label: 'Balance',
+				  data: dailyBalance, 
+				  borderColor: '#a6adbb',
+				  fill: false,
+				  stepped:true,
+				  pointRadius: 0,
+				}]
+			  };
+	  
+			  new Chart(ctx, {
+				type: 'line',
+				data: chartData,
+				options: {
+					layout: {
+						padding: 2
+					},
+					plugins: {
+						legend: {
+							display: false,
+						},
+						tooltip: {
+							enabled: false,
+						}
+					},
+					scales: {
+						x: {
+							grid: {
+								display: false,
+							},
+							ticks: {
+								display: false,
+							},
+							border:{
+								width:0
+							}
+						},
+						y: {
+							beginAtZero: true,
+							min: 0,
+							grid: {
+								display: false,
+							}, ticks: {
+								font: {
+									family: 'ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+									weight:'bolder'
+								}
+							},
+							border:{
+								width:0
+							}
+						}
+					}
+				}
+			  });
+			}
+		  },
+		  error: (error) => console.error('Error fetching transactions:', error),
+		  complete: () => console.info('Completed fetching transactions:')
+		});
+	  }
+	  
 }
